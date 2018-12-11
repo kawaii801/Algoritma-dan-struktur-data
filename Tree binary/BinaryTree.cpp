c687850a07376bacf98b7810fab00010d911c844
nardiyansah <janatri1398@gmail.com>
2018-12-11 17:04:47 +0700

    Update BinaryTree.cpp

--- Tree binary/BinaryTree.cpp
+++ Tree binary/BinaryTree.cpp
@@ -16,15 +16,15 @@ typedef address Tree;
 #define Left(P) (P)->left
 #define Right(P) (P)->right
 
-void CreateEmpty(Tree *P){
+void CreateEmpty(Tree *P){ //membuat tree kosong
     *P = NULL;
 }
 
-bool IsEmpty(Tree P){
+bool IsEmpty(Tree P){ //mengecek apakah tree kosong
     return (P == NULL);
 }
 
-address Allocation(infotype x){
+address Allocation(infotype x){ //membuat node baru
     address P = (ElmtTree*)malloc(sizeof(ElmtTree));
     Info(P) = x;
     Right(P) = NULL;
@@ -33,11 +33,11 @@ address Allocation(infotype x){
     return P;
 }
 
-void Deallocation(address hapus){
+void Deallocation(address hapus){ //dealokasi suatu node
     free(hapus);
 }
 
-bool IsOneElmt(Tree P){
+bool IsOneElmt(Tree P){ //mengecek apakah tree hanya satu elemen, tidak punya kaki sebelah kiri dan kanan
     if(P != NULL){
         return(Right(P) == NULL && Left(P) == NULL);
     }else{
@@ -45,7 +45,7 @@ bool IsOneElmt(Tree P){
     }
 }
 
-bool IsUnerLeft(Tree T){
+bool IsUnerLeft(Tree T){ //ngecek apa kaki kiri ada dan kaki kanan tidak ada
     if(T != NULL){
         return(Left(T) != NULL && Right(T) == NULL);
     }else{
@@ -53,7 +53,7 @@ bool IsUnerLeft(Tree T){
     }
 }
 
-bool IsUnerRight(Tree T){
+bool IsUnerRight(Tree T){ //ngecek apa kaki kanan ada dan kiri tidak ada
     if(T != NULL){
         return(Left(T) == NULL && Right(T) != NULL);
     }else{
@@ -61,7 +61,7 @@ bool IsUnerRight(Tree T){
     }
 }
 
-bool IsBiner(Tree T){
+bool IsBiner(Tree T){ //ngecek apa dua kaki ada semua
     if(T != NULL){
         return(Left(T) != NULL && Right(T) != NULL);
     }else{
@@ -69,7 +69,7 @@ bool IsBiner(Tree T){
     }
 }
 
-int JumlahElmt(Tree T){
+int JumlahElmt(Tree T){ //menghitung banyaknya elemen
     if(IsEmpty(T)){
         return 0;
     }else{
@@ -77,7 +77,7 @@ int JumlahElmt(Tree T){
     }
 }
 
-int JumlahDaun(Tree T){
+int JumlahDaun(Tree T){ //menghitung banyaknya daun (node paling bawah yang tidak punya kaki)
     if(IsOneElmt(T)){
         return 1;
     }else{
@@ -91,7 +91,35 @@ int JumlahDaun(Tree T){
     }
 }
 
-void AddBinaryTree(Tree *P, infotype x){
+void InsertLeft(Tree *P, infotype x, infotype base){ //memasukkan node baru hasil alokasi x di sebelah kiri node yang berinfo kan base
+    if(*P == NULL){
+
+    }else if(Info(*P) == base){
+            if(Left(*P) == NULL){
+                address temp = Allocation(x);
+                Left(*P) = temp;
+            }   
+    }else{
+        InsertLeft(&(Left(*P)),x,base);
+        InsertLeft(&(Right(*P)),x,base);
+    }
+}
+
+void InserRight(Tree *P, infotype x, infotype base){ //memasukkan node baru hasil alokasi x di sebelah kanan node yang berinfo kan base
+    if(*P == NULL){
+
+    }else if(Info(*P) == base){
+            if(Right(*P) == NULL){
+                address temp = Allocation(x);
+                Right(*P) = temp;
+            }   
+    }else{
+        InserRight(&(Right(*P)),x,base);
+        InserRight(&(Left(*P)),x,base);
+    }
+}
+
+void AddBinaryTree(Tree *P, infotype x){ //memasukkan node secara binary
     if(IsEmpty(*P)){
         address temp = Allocation(x);
         *P = temp;
@@ -134,28 +162,5 @@ void PrintPostOrder(Tree T){
 }
 
 int main(){
-    Tree T;
-    CreateEmpty(&T);
-
-    AddBinaryTree(&T,30);
-    AddBinaryTree(&T,10);
-    AddBinaryTree(&T,5);
-    AddBinaryTree(&T,17);
-    AddBinaryTree(&T,2);
-    AddBinaryTree(&T,3);
-    AddBinaryTree(&T,20);
-    AddBinaryTree(&T,13);
-    AddBinaryTree(&T,15);
-    AddBinaryTree(&T,7);
-    AddBinaryTree(&T,12);
-    cout << "PreOrder" << endl;
-    PrintPreOrder(T);
-    cout << endl << "InOrder" << endl;
-    PrintInOrder(T);
-    cout << endl << "PostOrder" << endl;
-    PrintPostOrder(T);
-    
-    cout << endl << "Jumlah Elment : " << JumlahElmt(T);
-    cout << endl << "Jumlah Daun : " << JumlahDaun(T);
     return 0;
 }
